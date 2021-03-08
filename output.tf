a26beddb905547bf67f2e847bff032e1bcbf6fa1
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2021-03-08 15:51:07 +0200

    add connection string as an output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "servicebus_namespace_name" {
     value = azurerm_servicebus_namespace.service_bus.name
 }
+
+output "service_bus_connection_string" {
+    value = azurerm_servicebus_namespace.service_bus.default_primary_connection_string
+}
