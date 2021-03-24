6fd0f92af7564ebcd534ec1921ef9a258511564d
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2021-03-24 09:46:55 +0200

    export primary key

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "servicebus_namespace_name" {
 output "service_bus_connection_string" {
     value = azurerm_servicebus_namespace.service_bus.default_primary_connection_string
 }
+
+output "service_bus_primary_key" {
+    value = azurerm_servicebus_namespace.service_bus.default_primary_key
+}
